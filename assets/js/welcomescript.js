d1809763ca5e6ce1f9522d083f2a2d5bf65fe848
marcod08 <you@example.com>
2023-11-14 12:34:07 +0100

    aggiunta funzioni per il collegamento al questionpage e cambio classe del bottone

---
+++ assets/js/welcomescript.js
@@ -0,0 +1,25 @@
+const buttonProceed = document.getElementById("button")
+const checkboxPromise = document.getElementById("promise")
+
+function cambiaColoreBottone () {
+    if (checkboxPromise.checked) {
+        buttonProceed.classList.remove("bottoneDisattivato");
+        buttonProceed.classList.add("bottoneAttivato");
+    } else {
+        buttonProceed.classList.remove("bottoneAttivato");
+        buttonProceed.classList.add("bottoneDisattivato");
+    }
+}
+
+function iniziaTest () {
+    buttonProceed.addEventListener("click", function () {
+        if (checkboxPromise.checked) {
+            window.location.href = "questionpage.html";
+        } else {
+            alert ("devi accettare le condizioni per procedere")
+        }
+    })
+
+}
+cambiaColoreBottone()
+iniziaTest()
