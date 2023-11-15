2ad0c8d7d372b8c1df77a5dc5afca50eca53293a
marcod08 <you@example.com>
2023-11-15 10:26:27 +0100

    funzione per la selezione stella migliorata, da integrare il salvataggio del record

--- assets/js/feedbackscript.js
+++ assets/js/feedbackscript.js
@@ -1,29 +1,28 @@
 const stelle = document.querySelectorAll(".stellaFeedback") // stelle recuperate con la classe stelle
 
+// ciclo per aumentare la luminosità delle stelle quando ci passi sopra con il mouse
 for (let i = 0; i < stelle.length; i++) {
     const stella = stelle[i]; //definisco stella come un elemento dell'array stelle
     stella.addEventListener("mouseover", function () {
-        cambiaLuminosità(i); // ciclo per dare cambiaLuminosità
+        aumentaLuminosità(i);
     })
-    stella.addEventListener("mouseout", function () {
-        ripristinaLuminosità(); // ciclo for per ripristinare la luminosità 
-    });
 }
-// funzione per ridurre la luminosità delle stelle a destra del mouse
-function cambiaLuminosità(index) {
+// funzione per aumentare la luminosità delle stelle 
+function aumentaLuminosità(index) {
     for (let i = 0; i < stelle.length; i++) {
         const stella = stelle[i];
-        if (i >= index) {
-            stella.style.filter = 'brightness(0.3)';
+        if (i > index) {
+            stella.style.filter = 'brightness(0.1)';
         } else {
             stella.style.filter = 'brightness(1)';
         }
     }
 }
-// funzione che ripristina la luminosità iniziale
-function ripristinaLuminosità() {
+// funzione che riduce la luminosità delle stelle
+function riduciLuminosità() {
     for (let i = 0; i < stelle.length; i++) {
         const stella = stelle[i];
-        stella.style.filter = 'brightness(1)';
+        stella.style.filter = 'brightness(0.1)';
     }
 }
+riduciLuminosità()
