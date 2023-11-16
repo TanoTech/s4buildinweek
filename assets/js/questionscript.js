956c91be9ba19df502399bae56508a4b0679dce2
GaetanoNapoli <gaetano.napoli@hotmail.com>
2023-11-16 12:02:28 +0100

    bugfix2

--- assets/js/questionscript.js
+++ assets/js/questionscript.js
@@ -393,20 +393,48 @@ const mostraRisultato = function () {
 
   contenitoreRateUs.appendChild(buttonRateUs)
 
-  let progressoValore = 0
-  let progressoValoreFinale = percentualeSbagliateRisposta
-  let velocitàProgresso = 25;
+  const animazioneProgressoPositivo = function () {
+    let progressoValore = 0
+    let progressoValoreFinale = percentualeSbagliateRisposta;
+    let velocitàProgresso = 20
 
-  let progress = setInterval(() => {
-    progressoValore++
-    risultatoEsameProgresso.style.background = `conic-gradient(
-      #B22B8A ${progressoValore * 3.6}deg,
-      #75FBFD ${progressoValore * 3.6}deg)`
+    let progress = setInterval(() => {
+      progressoValore++
+      risultatoEsameProgresso.style.background = `conic-gradient(
+        #B22B8A ${progressoValore * 3.6}deg,
+        #75FBFD ${progressoValore * 3.6}deg)`
 
-    if (progressoValore == progressoValoreFinale) {
-      clearInterval(progress)
-    }
-  }, velocitàProgresso)
+      if (progressoValore === progressoValoreFinale) {
+        clearInterval(progress)
+      }
+
+    }, velocitàProgresso)
+  }
+
+  const animazioneProgressoNegativo = function () {
+    let progressoValore = 0
+    let progressoValoreFinale = percentualeCorretteRisposta
+    let velocitàProgresso = 20
+
+    let progress = setInterval(() => {
+      progressoValore++
+      risultatoEsameProgresso.style.background = `conic-gradient(
+        #B22B8A ${progressoValore * 3.6}deg,
+        #75FBFD ${progressoValore * 3.6}deg)`
+
+      if (progressoValore === progressoValoreFinale) {
+        clearInterval(progress)
+      }
+
+    }, velocitàProgresso)
+  }
+
+  if (percentualeCorretteRisposta >= 60) {
+    animazioneProgressoPositivo()
+  } else {
+    animazioneProgressoNegativo()
+  }
+
 
 }
 
