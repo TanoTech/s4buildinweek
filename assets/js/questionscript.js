9f57346903cbb449a652b04fd001eaa08d27402e
Matteo <matteo.3000@outlook.it>
2023-11-17 11:55:32 +0100

    piccole modifiche results

--- assets/js/questionscript.js
+++ assets/js/questionscript.js
@@ -340,7 +340,7 @@ const mostraRisultato = function () {
   correctP.textContent = "Correct"
   let percentualeCorrette = document.createElement('p')
   percentualeCorrette.classList.add('percentualeCorrette')
-  percentualeCorrette.textContent = `${percentualeCorretteRisposta} %`
+  percentualeCorrette.textContent = `${percentualeCorretteRisposta}%`
   let indiceCorrette = document.createElement('p')
   indiceCorrette.textContent = `${risposteCorretteLunghezza}/15 questions`
 
@@ -374,7 +374,7 @@ const mostraRisultato = function () {
   const wrongP = document.createElement('p')
   wrongP.textContent = "Wrong"
   let percentualeSbagliate = document.createElement('p')
-  percentualeSbagliate.textContent = `${percentualeSbagliateRisposta} %`
+  percentualeSbagliate.textContent = `${percentualeSbagliateRisposta}%`
   percentualeSbagliate.classList.add('percentualeSbagliate')
   let indiceSbagliate = document.createElement('p')
   indiceSbagliate.textContent = `${risposteSbagliateLunghezza}/15 questions`
