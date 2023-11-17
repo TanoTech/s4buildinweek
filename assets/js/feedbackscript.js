27977c263411332ba9cdb4f090ab06667a9b46e3
GaetanoNapoli <gaetano.napoli@hotmail.com>
2023-11-17 13:57:50 +0100

    check codici, funzionalità, aspetto

--- assets/js/feedbackscript.js
+++ assets/js/feedbackscript.js
@@ -1,115 +1,122 @@
-const tooltip = document.getElementById("tooltip2") //tooltip se utente non seleziona le stelle
-const stelle = document.querySelectorAll(".stellaFeedback");
-const stelleSelezionate = []
-let ultimaImmagineCasuale = null
+const tooltip = document.getElementById("tooltip2")
+        const stelle = document.querySelectorAll(".stellaFeedback")
+        const stelleSelezionate = []
+        let ultimaImmagineCasuale = null
+        let immaginiProfilo = [
+            "assets/immagini/tottiPic.jpg",
+            "assets/immagini/deRossi.jpeg",
+            "assets/immagini/murino.jpeg",
+            "assets/immagini/spalletti.jpeg",
+            "assets/immagini/spalletti2.jpeg",
+            "assets/immagini/zampa.jpeg",
+            "assets/immagini/doggo.jpg",
+            "assets/immagini/hackerino.jpg",
+            "assets/immagini/download.jpeg",
+        ];
+        let immaginiDisponibili = [...immaginiProfilo]
 
-for (let i = 0; i < stelle.length; i++) {
-    const stella = stelle[i]
-    stella.addEventListener("mouseover", function () {
-        aumentaLuminosità(i)
-    });
+        riduciLuminosità()
 
-    stella.addEventListener("click", function () {
-        aggiungiValore(i)
-    });
-}
+        for (let i = 0; i < stelle.length; i++) {
+            const stella = stelle[i]
 
-function aumentaLuminosità(index) {
-    for (let i = 0; i < stelle.length; i++) {
-        const stella = stelle[i];
-        if (i > index) {
-            stella.style.filter = 'brightness(0.1)'
-        } else {
-            stella.style.filter = 'brightness(1)'
+            stella.addEventListener("mouseover", function () {
+                aumentaLuminosità(i)
+            });
+
+            stella.addEventListener("mouseout", function () {
+                if (stelleSelezionate.indexOf(i + 1) === -1) {
+                    riduciLuminosità()
+                }
+            });
+
+            stella.addEventListener("click", function () {
+                aggiungiValore(i)
+                aumentaLuminosità(i)
+            });
         }
-    }
-}
 
-function riduciLuminosità() {
-    for (let i = 0; i < stelle.length; i++) {
-        const stella = stelle[i];
-        stella.style.filter = 'brightness(0.1)'
-    }
-}
+        let inputCommenti = document.getElementById("inputCommenti");
 
-function aggiungiValore(index) {
-    const valoreStella = index + 1
-
-    if (valoreStella >= 1 && valoreStella <= 10) {
-        if (stelleSelezionate.length === 0) {
-            stelleSelezionate.push(valoreStella)
-        } else {
-            stelleSelezionate.shift()
-            stelleSelezionate.push(valoreStella)
+        function recuperaValoreInput() {
+            return inputCommenti.value
         }
-    }
-    console.log(stelleSelezionate)
-}
 
-let inputCommenti = document.getElementById("inputCommenti")
+        let bottoneMoreInfo = document.querySelector("button");
 
-function recuperaValoreInput() {
-    return inputCommenti.value
-}
+        bottoneMoreInfo.addEventListener("click", function () {
+            if (stelleSelezionate.length > 0 && immaginiDisponibili.length > 0) {
+                let mainTag = document.querySelector("main")
+                let divFeedback = document.createElement("div")
+                let divPic = document.createElement("div")
+                let profilePic = document.createElement("img")
 
-let bottoneMoreInfo = document.querySelector("button")
+                let indiceImmagineCasuale;
+                if (immaginiDisponibili.length > 0) {
+                    indiceImmagineCasuale = Math.floor(Math.random() * immaginiDisponibili.length)
+                    ultimaImmagineCasuale = immaginiDisponibili[indiceImmagineCasuale]
+                    immaginiDisponibili.splice(indiceImmagineCasuale, 1)
+                }
 
+                profilePic.src = ultimaImmagineCasuale
 
-const immaginiProfilo = [
-    "assets/immagini/tottiPic.jpg",
-    "assets/immagini/deRossi.jpeg",
-    "assets/immagini/murino.jpeg",
-    "assets/immagini/spalletti.jpeg",
-    "assets/immagini/spalletti2.jpeg",
-    "assets/immagini/zampa.jpeg",
-    "assets/immagini/doggo.jpg",
-    "assets/immagini/hackerino.jpg",
-    "assets/immagini/download.jpeg",
-]
+                let divCommento = document.createElement("div")
+                let paraCommento = document.createElement("p")
+                let stelleCommento = document.createElement("img")
+                stelleCommento.src = "assets/immagini/star.svg"
 
-bottoneMoreInfo.addEventListener("click", function () {
-    if (stelleSelezionate.length > 0) {
-        let mainTag = document.querySelector("main")
-        let divFeedback = document.createElement("div")
-        let divPic = document.createElement("div")
-        let profilePic = document.createElement("img")
+                paraCommento.innerText = recuperaValoreInput()
 
+                for (let i = 0; i < stelleSelezionate[0]; i++) {
+                    divCommento.appendChild(stelleCommento.cloneNode())
+                }
 
-        let indiceImmagineCasuale;
-        do {
-            indiceImmagineCasuale = Math.floor(Math.random() * immaginiProfilo.length)
-        } while (indiceImmagineCasuale === ultimaImmagineCasuale)
+                divCommento.appendChild(paraCommento)
+                divPic.appendChild(profilePic)
+                divFeedback.appendChild(divPic)
+                divFeedback.appendChild(divCommento)
+                mainTag.appendChild(divFeedback)
 
-        ultimaImmagineCasuale = indiceImmagineCasuale
+                divFeedback.id = "divFeedback"
+                divPic.id = "divPic"
+                profilePic.id = "profilePic"
+                divCommento.id = "divCommento"
+                paraCommento.id = "paraCommento"
+                stelleCommento.id = "stelleCommento"
+                tooltip.style.display = "none"
+            } else {
+                tooltip.style.display = "block"
+            }
+        });
 
-        profilePic.src = immaginiProfilo[indiceImmagineCasuale]
+        function aumentaLuminosità(index) {
+            for (let i = 0; i < stelle.length; i++) {
+                const stella = stelle[i];
+                if (i > index) {
+                    stella.style.filter = 'brightness(0.1)'
+                } else {
+                    stella.style.filter = 'brightness(1)'
+                }
+            }
+        }
 
-        let divCommento = document.createElement("div")
-        let paraCommento = document.createElement("p")
-        let stelleCommento = document.createElement("img")
-        stelleCommento.src = "assets/immagini/star.svg"
+        function riduciLuminosità() {
+            for (let i = 0; i < stelle.length; i++) {
+                const stella = stelle[i]
+                stella.style.filter = 'brightness(0.1)'
+            }
+        }
 
-        paraCommento.innerText = inputCommenti.value
+        function aggiungiValore(index) {
+            const valoreStella = index + 1
 
-        for (i = 0; i < stelleSelezionate[0]; i++) {
-
-            divCommento.appendChild(stelleCommento.cloneNode()); // ciclo per appendere piu stelle
-        };
-        divCommento.appendChild(paraCommento); // appendo il paragrafo al divCommento
-        divPic.appendChild(profilePic); // appendo la foto al il DivPic
-        divFeedback.appendChild(divPic); // appendo i 2 div interni al divFeedback
-        divFeedback.appendChild(divCommento);
-        mainTag.appendChild(divFeedback); // appendo finalmente il tutto al main
-        // assegnazione ID ai vari elementi per formattarli in css
-        divFeedback.id = "divFeedback";
-        divPic.id = "divPic";
-        profilePic = "profilePic";
-        divCommento.id = "divCommento";
-        paraCommento.id = "paraCommento";
-        stelleCommento.id = "stelleCommento";
-        tooltip.style.display = "none" // nasconde il tooltip se viene selezionato un rating
-    } else {
-        tooltip.style.display = "block"; // mostra il tooltip per la selezione del rating}
-})
-
-riduciLuminosità()
+            if (valoreStella >= 1 && valoreStella <= 10) {
+                if (stelleSelezionate.length === 0) {
+                    stelleSelezionate.push(valoreStella)
+                } else {
+                    stelleSelezionate.shift();
+                    stelleSelezionate.push(valoreStella)
+                }
+            }
+            console.log(stelleSelezionate)
+        }
